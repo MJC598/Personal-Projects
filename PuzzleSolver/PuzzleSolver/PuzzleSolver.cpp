716e41c40f06f46a849202318151361e7167cddc
Matthew Carroll <matthew.j.carroll@outlook.com>
2018-09-30 21:59:27 -0500

    started PuzzleSolver Project

---
+++ PuzzleSolver/PuzzleSolver/PuzzleSolver.cpp
@@ -0,0 +1,12 @@
+﻿// PuzzleSolver.cpp : Defines the entry point for the application.
+//
+
+#include "PuzzleSolver.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ PuzzleSolver/PuzzleSolver/PuzzleSolver.h
@@ -0,0 +1,26 @@
+﻿// PuzzleSolver.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+class Node {
+private:
+	int id;
+	Node *parent;
+	vector<Node *>children;
+
+public:
+	//getters
+	int getId() { return id; }
+	Node* getParent() { return parent; }
+	vector<Node *> getChildren() { return children; }
+	//setters
+	void setId(int id) { this->id = id; }
+	void setParent(Node *parent) { this->parent = parent; }
+	void setChildren(vector<Node *> children) { this->children = children; }
+};
+
+// TODO: Reference additional headers your program requires here.
